99cc71b317f7357e28fa7c3032771a6e3f6fdd3c
Elavii <elias.viitanen@hotmail.com>
2023-01-24 20:32:38 +0200

    1.6 tehty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = (e) => {
+    e.preventDefault();
+    setGood(good + 1);
+  }
+
+  const handleNeutralClick = (e) => {
+    e.preventDefault();
+    setNeutral(neutral + 1);
+  }
+
+  const handlleBadClick = (e) => {
+    e.preventDefault();
+    setBad(bad + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handlleBadClick}>bad</button>
+      <h1>statistics</h1>
+      <p style={{margin: 0}}>good {good}</p>
+      <p style={{margin: 0}}>neutral {neutral}</p>
+      <p style={{margin: 0}}>bad {bad}</p>
     </div>
-  );
+  )
 }
 
 export default App;
